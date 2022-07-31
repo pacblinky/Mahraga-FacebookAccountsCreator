15583e1efeaa3457e91d5f9833d3e4477779e90d
pacblinky <40526207+pacblinky@users.noreply.github.com>
2022-07-31 16:42:55 -0600

    baby sql come back

---
+++ sql.py
@@ -0,0 +1,23 @@
+import mariadb
+
+class SQL:
+    def __init__(self,host,port,username,password,database):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.cursor = None
+        self.db = None
+
+    def connect(self):
+        try:
+            self.db = mariadb.connect(user=self.username,password=self.password,host=self.host,port=self.port,database=self.database)
+            self.cursor = self.db.cursor()
+            return True
+        
+        except Exception:
+            return False
+
+    def query(self,sql,params = []):
+        self.cursor.execute(sql,params)
